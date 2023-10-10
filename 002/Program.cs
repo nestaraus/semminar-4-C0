3f74e4c7fd4a87aa7123af798152579e48f9a7d4
nestaraus <nestaraus@mail.pu>
2023-10-10 21:20:46 +0300

    Zadacha 2

---
+++ 002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу замена элементов массива: положительные элементы массива замените на отрицательные и наоборот
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+}
+
+void ReleaseArray(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+   
+    
+}
+
+Console.Clear();
+Console.Write("Размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
